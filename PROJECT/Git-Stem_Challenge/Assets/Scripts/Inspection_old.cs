6d728ab66eb4273d4bbcf9df9cba397681a8b6f0
Dylan Gonzalez <dylan.claudio.g@gmail.com>
2019-01-07 18:39:36 +1100

    Inspection System Additions
    Blur, FOV, Rotation

--- PROJECT/Git-Stem_Challenge/Assets/Scripts/Inspection_old.cs
+++ PROJECT/Git-Stem_Challenge/Assets/Scripts/Inspection_old.cs
@@ -1,40 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityStandardAssets.ImageEffects;
 
-public class Inspection : MonoBehaviour
+public class Inspection_old : MonoBehaviour
 {
     CharacterControllerSub charContr;
-    public CharacterMotor motor;
-    OrbitController orbitContr;
-    DepthOfField DOF;
 
     public bool grab = false;
 
     public Transform inspectionPos;
-    public Camera cam;
-    public Canvas canvas;
-    GameObject item;
+    public CharacterMotor motor;
 
+    public GameObject item;
+    public Camera cam;
 
     float startTime = 0;
     float waitTime = 0.5f;
     public float force = 10f;
-
     public float playerImpactingForce = 4f;
-    public float rotSpeed = 20f;
 
+    public Vector3 originalPos = new Vector3(0, 0, 0);
 
-    Vector3 originalPos;
-    Quaternion originalRot;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         charContr = gameObject.GetComponent<CharacterControllerSub>();
-        orbitContr = gameObject.GetComponentInChildren<OrbitController>();
-        DOF = gameObject.GetComponentInChildren<DepthOfField>();
     }
 
     void Update()
@@ -43,18 +34,16 @@ public class Inspection : MonoBehaviour
         Debug.DrawLine(ray.origin, cam.transform.forward * 10000, Color.red);
         RaycastHit hit;
 
-        if (!grab && Physics.Raycast(ray, out hit) && hit.distance < 3f)
+        if (!grab && Physics.Raycast(ray, out hit))
         {
             if (hit.transform.tag == "Item" || hit.transform.tag == "Item 2" && hit.distance < 3f)
             {
-                if (Input.GetKeyDown(KeyCode.F))
+                if (Input.GetKeyDown(KeyCode.Mouse0))
                 {
                     grab = true;
                     item = hit.transform.gameObject;
                     startTime = Time.time;
                     originalPos = item.transform.position;
-                    originalRot = item.transform.rotation;
-                    canvas.enabled = false;
                 }
             }
         }
@@ -73,16 +62,11 @@ public class Inspection : MonoBehaviour
             if (item.transform.tag == "Item 2")
             {
                 charContr.movementSpeed = 0;
-                orbitContr.enabled = false;
-
-                cam.fieldOfView = 40;
-                DOF.focalTransform = item.transform;
-                DOF.aperture = 0.6f;
             }
 
             if (Time.time - startTime > waitTime)
             {
-                if (Input.GetKeyDown(KeyCode.F))
+                if (Input.GetKeyDown(KeyCode.Mouse0))
                 {
                     item.GetComponent<Rigidbody>().detectCollisions = true;
                     item.transform.parent = null;
@@ -93,41 +77,17 @@ public class Inspection : MonoBehaviour
 
                     if (item.transform.tag == "Item 2")
                     {
-                        item.transform.rotation = originalRot;
                         item.transform.position = originalPos; //snap item back to original position
                         charContr.movementSpeed = 6;
-                        orbitContr.enabled = true;
-                        cam.fieldOfView = 70;
-                        DOF.focalTransform = null;
-                        DOF.aperture = 0f;
-
-                        Cursor.visible = false;
-                        Cursor.lockState = CursorLockMode.Locked;
                         
                     }
-
                     if (item.transform.tag == "Item")
                     {
                         item.GetComponent<Rigidbody>().AddForce(cam.transform.forward * force + motor.rBody.velocity*playerImpactingForce);
 
                     }
-
-                    canvas.enabled = true;
                 }
-            }
-
-            if (Input.GetMouseButton(0) && item.transform.tag == "Item 2")
-            {
-                float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
-                float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
-
-                item.transform.Rotate(cam.transform.up, rotX, Space.World);
-                item.transform.Rotate(cam.transform.right, rotY, Space.World);
-
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
             }
         }
     }
-
 }
